950101cf1b84b57a50dd01baee365a8427c8b823
newjini <kimyjin0920@gmail.com>
2022-04-29 00:11:17 +0900

    refactor: 엔티티 설계시 주의사항 추가
    지연로딩, cascade, 연관관계 메서드 추가

--- jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -3,11 +3,14 @@ package jpabook.jpashop.domain;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.boot.orm.jpa.hibernate.SpringPhysicalNamingStrategy;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+
+import static javax.persistence.FetchType.LAZY;
 
 @Entity
 @Table(name = "orders")
@@ -18,15 +21,14 @@ public class Order {
     @Column(name = "order_id")
     private Long id;
 
-
-    @ManyToOne
+    @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "member_id") // foreign key 는 member_id 라는 셈.
     private Member member;          // 회원과 주문 사이의 연관관계 주인.
 
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne
+    @OneToOne(fetch = LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
@@ -34,5 +36,22 @@ public class Order {
 
     @Enumerated(EnumType.STRING)
     private OrderStatus status;         // 주문상태 [ORDER, CANCEL]
+
+
+    //==연관관계 (편의) 메서드==//
+    public void setMember(Member member) {
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery){
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
 
 }
